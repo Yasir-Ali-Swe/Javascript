e850cc322447e5bfdb8ad4f91fced5f9ce6f3e84
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-08-25 16:01:29 +0500

    Let,var,const,variables and datatypes.

---
+++ Exercise1.js
@@ -0,0 +1,26 @@
+console.log("Hell Js");
+console.log("let: Declares a block-scoped variable that can be reassigned.");
+console.log(
+  "const: Declares a block-scoped variable that cannot be reassigned after its initial assignment."
+);
+console.log(
+  "var: Declares a function-scoped or globally-scoped variable that can be reassigned and is hoisted"
+);
+
+let num = 123;
+console.log("type of Num is " + typeof num);
+
+let text = "123";
+console.log("type of text is " + typeof text);
+
+let isAdult = true;
+console.log("type of isAdult is " + typeof isAdult);
+
+let list = [1, 2, 3, "Yasir Ali"];
+console.log("Type of list is " + typeof list);
+
+let obj = {
+  name: "Yasir Ali",
+  Gender: "Male",
+};
+console.log("Type of obj is " + typeof obj);
