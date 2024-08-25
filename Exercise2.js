a29083bd8212149346081f95c954f05852848b24
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-08-25 16:31:06 +0500

    Operatore in js

---
+++ Exercise2.js
@@ -0,0 +1,18 @@
+console.log("Operators");
+
+let a = 5;
+let b = 3;
+console.log("Sum is ", a + b);
+console.log("Suntraction  is ", a - b);
+console.log("Multiply is ", a * b);
+console.log("Division is ", Math.ceil(a / b));
+console.log("Modulus is ", a % b);
+console.log("Value of A is -> ", a);
+console.log("Pre increment in a ", ++a);
+console.log("Value of A is -> ", a);
+console.log("Pre decrement in a ", --a);
+console.log("Value of A is -> ", a);
+console.log("Post increment in a ", a++);
+console.log("Value of A is -> ", a);
+console.log("Post decrement in a ", a--);
+console.log("Value of A is -> ", a);
