5a904701d5c758b43d8001977d56ae22997cc0cd
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-08-26 14:19:59 +0500

    Array map method

---
+++ Exercise8.js
@@ -0,0 +1,10 @@
+let array = [1, 2, 3, 4, 5];
+let newArray = array.map((item) => {
+  return item * item;
+});
+console.log(newArray);
+let arraySum = 0;
+array.map((item) => {
+  arraySum += item;
+});
+console.log(arraySum);
