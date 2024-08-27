a57bdc192e9858c3f24f8a30d3088ce8a78ad3e2
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-08-27 15:59:44 +0500

    Array methods

---
+++ Exercise12.js
@@ -0,0 +1,45 @@
+let arrayA = [1, 2, 3, 4, 5, 5, 18, 9, 16, 6, -1];
+let arrayB = [6, 7, 8, 9, 10];
+// Array Concatination
+// let arrayC = arrayA.concat(arrayB);
+
+// Array push method
+// arrayA.push(6);
+// console.log(arrayA);
+
+// Array pop method
+// arrayA.pop();
+// console.log(arrayA);
+
+// Array unshift method
+// arrayA.unshift(0);
+// console.log(arrayA);
+
+// Array shift method
+// arrayA.shift();
+// console.log(arrayA);
+
+// Array slice method
+// console.log(arrayC);
+// let newSlice = arrayA.slice(2, 5);
+// console.log(arrayA);
+// console.log(newSlice);
+// Array splice method
+// let newSplice = arrayA.splice(0, 3, -3);
+// console.log(
+//   "after deleting element from original array the original array is this ",
+//   arrayA
+// );
+// console.log("Deleted element from original array is ", newSplice);
+
+// Array firstindex method
+// console.log(arrayA.findIndex((item) => item === 5));
+
+// Array Last index method
+// console.log(arrayA.lastIndexOf(5));
+
+// array length method
+// console.log(arrayA.length);
+
+// Array sort method
+console.log(arrayA.sort((a, b) => a - b));
