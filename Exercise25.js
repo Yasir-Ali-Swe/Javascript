0086ce7d289d9802e25816364579cb705862a304
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-09-01 21:44:53 +0500

    Plan functions and Arrow Functions

---
+++ Exercise25.js
@@ -0,0 +1,11 @@
+// function greetings(name) {
+//   return `Good morning ${name}.`;
+// }
+
+// console.log(greetings("Yasir Ali"));
+
+let greetings = (name) => {
+  return `Good morning ${name}.`;
+};
+
+console.log(greetings("Yasir Ali"));
