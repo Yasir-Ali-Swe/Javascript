6f7c467dd222066730a7c990f4d2b2f6e5df76ed
Yasir-Ali-Swe <ali.yasirswe@gmail.com>
2024-09-06 17:28:14 +0500

    Itrate over the array of objects

--- Exercise42.js
+++ Exercise42.js
@@ -2,5 +2,11 @@ let apiUrl = "https://api.coincap.io/v2/assets";
 (async function fetchData() {
   let data = await fetch(apiUrl);
   data = await data.json();
-  console.log(data);
+  data = data.data;
+  data.forEach((item) => {
+    for (let key in item) {
+      console.log(`${key}      ${item[key]}.`);
+    }
+    console.log("\n");
+  });
 })();
